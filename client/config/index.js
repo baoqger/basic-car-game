76358b311f54506e2061926280036cd8910f6dc8
jingjing.bao <jingjing.bao@tendcloud.com>
2018-02-06 10:28:26 +0800

    basic staff

---
+++ client/config/index.js
@@ -0,0 +1,4 @@
+export const WINDOW_WIDTH = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+export const WINDOW_HEIGHT = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+export const ASSET_URL = '../assets'
+export const WORLD_SIZE = { width: 1600, height: 1200 }
